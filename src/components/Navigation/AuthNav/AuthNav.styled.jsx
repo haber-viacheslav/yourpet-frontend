d6ca8303d7f44fbece756acced5b864777a69e52
VitaliySaburdo <vsaburdo@gmail.com>
2023-05-19 19:43:36 +0300

    styles

--- src/components/Navigation/AuthNav/AuthNav.styled.jsx
+++ src/components/Navigation/AuthNav/AuthNav.styled.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+import {theme} from '../../../theme/theme';
+
 export const AuthNavMenu = styled.div`
   display: flex;
   align-items: center;
@@ -7,6 +9,22 @@ export const AuthNavMenu = styled.div`
 `;
 
 export const StyledLink = styled(NavLink)`
+display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  width: 165px;
+  height: 40px;
+  cursor: pointer;
+  font-size: ${theme.fontSizes[2]}px;
+  color: ${theme.colors.orange};
+  background-color: transparent;
+  border: 2px solid ${theme.colors.orange};
+  border-radius: 40px;
+  outline: none;
+  /* transition-property: background-color;
+  transition-duration: 250ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1); */
   &:hover,
   &:focus {
   }
