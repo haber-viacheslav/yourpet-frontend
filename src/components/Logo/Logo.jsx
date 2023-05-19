c6b181726729d25a98f6389301657add11844ab2
DarinaPilganchuk <106483724+DarinaPilganchuk@users.noreply.github.com>
2023-05-19 09:19:35 +0300

    add breakpoints

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,18 @@
+
+// import { LogoWrapper } from './Logo.styled';
+
+// const Logo = () => {
+  
+
+//   return (
+//     <LogoWrapper to="/">
+//        <svg width={116} height={20}>
+//   <use href="./images/logo-small.svg"></use>
+// </svg> : <svg width={162} height={28}>
+//   <use href="./images/logo.svg"></use>
+// </svg>
+//     </LogoWrapper>
+//   );
+// };
+
+// export default Logo;
