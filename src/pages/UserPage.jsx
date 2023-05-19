c7225a184dcd5870a5a9423e8c7a74f123723cab
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-05-20 00:09:58 +0300

    container.jsx

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -2,10 +2,11 @@ import { UserData } from '../components/UserData/UserData';
 import { PetsData } from '../components/PetsData/PetsData';
 import { Logout } from '../components/Logout/Logout';
 import { AddPetBtn } from '../components/AddPetBtn/AddPetBtn';
+import { Container } from 'components/Container/Container';
 
 const UserPage = () => {
   return (
-    <>
+    <Container>
       <div>
         <UserData />
         <Logout />
@@ -17,7 +18,7 @@ const UserPage = () => {
       </div>
 
       <PetsData />
-    </>
+    </Container>
   );
 };
 
