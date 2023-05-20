a55277a88dc0038262956a1e2cfd4dc42045a71f
Oleg-Deda <dedaoleg1983@gmail.com>
2023-05-20 22:28:33 +0300

    main page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,14 @@
+// import { StyledContainer} from '../Container/Container.styled' ;
+import { HeroSection } from '../Hero/Hero.styled';
+// import { Header } from 'components/Header/Header';
+
 export const Hero = () => {
-  return;
+  return (
+    <>
+    <HeroSection>
+              <h1>Take good care of your small pets</h1>
+       <div></div>
+    </HeroSection>
+        </>
+  );
 };
