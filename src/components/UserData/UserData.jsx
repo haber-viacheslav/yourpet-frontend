f858ce4950c6882c3d57085d6b12c666a510c3da
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-05-20 13:18:01 +0300

    UserData ( begin writing)

--- src/components/UserData/UserData.jsx
+++ src/components/UserData/UserData.jsx
@@ -1,11 +1,23 @@
 // import { StyledInput, StyledLabel } from './UserData.styled';
+import styled from 'styled-components';
+import { FlexContainer } from 'components/FlexContainer/FlexContainer';
 import { UserDataItem } from '../UserDataItem/UserDataItem';
+
+const StyledProfileTitle = styled.h2`
+  font-family: Manrope;
+  font-style: Medium;
+  font-size: 20px;
+  line-height: calc(27/20);
+  letter-spacing: 4%;
+  color: rgba(0, 0, 0, 1);
+  align: left;
+`;
 
 export const UserData = () => {
   return (
-    <>
-      <h2>UserData Profile will be here</h2>
+    <FlexContainer>
+      <StyledProfileTitle>My information:</StyledProfileTitle>
       <UserDataItem />
-    </>
+    </FlexContainer>
   );
 };
