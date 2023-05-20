5ba9405e6b8f35019adcf360ce67c2c676b1fdf5
VitaliySaburdo <vsaburdo@gmail.com>
2023-05-20 07:19:30 +0300

    1

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,33 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Wrapper, Form } from './RegisterForm.styled';
-import {Input, Title, Box} from './RegisterForm.styled'
-
-
-
+import { Input, Title, Box, Button, Text, Span } from './RegisterForm.styled';
 
 export const RegisterForm = () => {
-
-
   return (
     <Wrapper>
       <Form>
         <Title>Registation</Title>
         <Box>
-        <Input type="text" name="email" placeholder="Email" />
-        <Input type="password" name="password" placeholder="Password" />
-        <Input type="password" name="confirm" placeholder="Confirm password"/>
+          <Input type="text" name="email" placeholder="Email" />
+          <Input type="password" name="password" placeholder="Password" />
+          <Input
+            type="password"
+            name="confirm"
+            placeholder="Confirm password"
+          />
         </Box>
+        <Button>Login</Button>
+        <Text>
+          Already have an account?
+          <Span>
+            <NavLink to="/login">Login</NavLink>
+          </Span>
+        </Text>
       </Form>
     </Wrapper>
   );
 };
-
-
-
-
-
-
-
-
-
-
--- src/components/RegisterForm/RegisterForm.styled.jsx
+++ src/components/RegisterForm/RegisterForm.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-// import second from '../../theme/theme'
+import {theme} from '../../theme/theme'
 
 export const Wrapper = styled.div`
   margin-left: auto;
@@ -32,12 +32,13 @@ export const Box = styled.div`
   gap: 24px;
   width: 100%;
   margin-top: 40px;
+  margin-bottom: 40px;
 `;
 
 export const Title = styled.h2`
-  font-family: 'Manrope';
+  font-family: ${theme.fonts.main};
   font-style: normal;
-  font-weight: 500;
+  font-weight: ${theme.fontWeights.medium};
   font-size: 24px;
   line-height: 33px;
   display: flex;
@@ -50,9 +51,9 @@ export const Input = styled.input`
   display: block;
   margin-left: auto;
   margin-right: auto;
-  font-family: 'Manrope';
+  font-family: ${theme.fonts.main};
   font-style: normal;
-  font-weight: 400;
+  font-weight: ${theme.fontWeights.regular};
   font-size: 16px;
   padding: 12px 16px;
   width: 100%;
@@ -60,4 +61,34 @@ export const Input = styled.input`
   border: 1px solid #54adff;
   border-radius: 40px;
   cursor: pointer;
+`;
+
+export const Button = styled.button`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  padding: 12px 16px;
+  width: 100%;
+  border: none;
+  border-radius: 40px;
+  cursor: pointer;
+  background: #54ADFF;
+  margin-bottom: 24px;
+  
+`;
+
+export const Text = styled.p`
+  font-family: ${theme.fonts.main};
+  font-style: normal;
+  font-weight: ${theme.fontWeights.regular};
+  font-size: 16px;
+  color: #888888;
+`;
+
+export const Span = styled.span`
+  margin-left: 3px;
 `;
