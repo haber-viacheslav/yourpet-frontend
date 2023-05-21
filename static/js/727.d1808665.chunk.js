633ae3b152dfaec9080d7fec2ea8bf5893e40c6b
haber-viacheslav <gaberslavik@gmail.com>
2023-05-21 08:30:55 +0000

    Deploying to gh-pages from @ haber-viacheslav/yourpet-frontend@7881afe3b21919315704776b14349124153a0cab 🚀

--- static/js/727.d1808665.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkyourpet_frontend=self.webpackChunkyourpet_frontend||[]).push([[727],{727:function(e,n,r){r.r(n);var t=r(184);n.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("h2",{children:"Main page"})})}}}]);
-//# sourceMappingURL=727.d1808665.chunk.js.map
