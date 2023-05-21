c14029ffef864db95d40d6bceb56087ff5494d92
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-05-22 00:19:40 +0300

    userAvatar files

---
+++ src/components/UserData/UserAvatarInput/UserAvatarInput.jsx
@@ -0,0 +1,37 @@
+import { 
+  AvatarInputLabel, 
+  AvatarInput,
+  LabelContainer 
+} from "./UserAvatarInput.styled";
+import { FlexContainer } from "components/FlexContainer/FlexContainer";
+
+import { SVG } from "../UserDataItem/UserDataItem.styled";
+import icons from 'images/icons.svg';
+
+
+export const UserAvatarInput = () => {
+    return (
+    <>
+      <AvatarInputLabel>
+        <AvatarInput  
+          type='file' 
+          name='avatar'
+          margin-bottom={'15px'}
+        />
+
+        <div>
+          <SVG width={20} height={20} stroke={'#00C3AD'}>
+            <use href={icons + '#icon-check'}></use>
+          </SVG>
+          <span>Edit photo</span>
+        </div>
+      </AvatarInputLabel>
+              <div>
+              <SVG width={20} height={20} stroke={'#00C3AD'}>
+                <use href={icons + '#icon-check'}></use>
+              </SVG>
+              <span>Confirm</span>
+            </div>
+    </>
+    );
+  };
