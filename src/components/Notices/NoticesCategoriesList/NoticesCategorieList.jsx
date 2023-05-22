5c39ffdf8ab495203135832572ad28fd3af39309
SerhiySBV <serhiybelyavskii@gmail.com>
2023-05-23 00:26:06 +0300

    LIst styled

--- src/components/Notices/NoticesCategoriesList/NoticesCategorieList.jsx
+++ src/components/Notices/NoticesCategoriesList/NoticesCategorieList.jsx
@@ -1,13 +1,14 @@
 import { NoticesCategoryItem } from 'components/Notices/NoticesCategoriesItem/NoticesCategoriesItem';
 import petsLists from '../../../mockData/petsList';
 import { nanoid } from '@reduxjs/toolkit';
+import { List } from './NoticesCategoriesList.styled';
 
 export const NoticesCategoriesList = () => {
   return (
-    <ul>
+    <List>
       {petsLists.map(petItem => (
         <NoticesCategoryItem petList={petItem} key={nanoid()} />
       ))}
-    </ul>
+    </List>
   );
 };
