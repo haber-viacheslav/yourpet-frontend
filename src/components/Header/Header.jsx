f124685f4112a52f24268d2fa61ef252dc9c2523
DarinaPilganchuk <106483724+DarinaPilganchuk@users.noreply.github.com>
2023-05-22 18:38:54 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,17 @@
-// import Nav from 'components/Navigation/Nav/Nav';
 import Nav  from 'components/Navigation/Nav/Nav';
 import { AuthNav } from 'components/Navigation/AuthNav/AuthNav';
 import { Logo } from 'components/Logo/Logo';
 import { HeaderWrapper } from './Header.styled';
-// import { BurgerMenu } from 'components/Navigation/BurgerMenu/BurgerMenu';
-
 export const Header = () => {
  
   return (
     <HeaderWrapper>
-        <Logo />
-      <Nav/>
+      <Logo />
+      <Nav />
       <AuthNav />
-
     </HeaderWrapper>
-  );
+
+  )
 };
+export default Header;
+
--- src/components/Navigation/Navigation.styled.jsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-const BtnGroup = styled.div 
-    `display: flex;
-`;
-  export {BtnGroup };
