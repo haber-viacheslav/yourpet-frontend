d425945c0ef74d1267d0d523f55b90faba7dc65f
SerhiySBV <serhiybelyavskii@gmail.com>
2023-05-23 03:25:27 +0300

    Title

--- src/pages/NoticesPage.jsx
+++ src/pages/NoticesPage.jsx
@@ -3,13 +3,16 @@ import { Container } from '../components/Container/Container';
 import { Section } from '../components/Section/Section';
 import { NoticesCategoriesNav } from 'components/Notices/NoticesCategoriesNav/NoticesCategoriesNav';
 import { NoticesSearch } from 'components/Notices/NoticesSerch/NoticesSerch';
+import { Title, Wrapper } from 'components/Friends/Friends.styled';
 
 const NoticesPage = () => {
   return (
     <>
       <Section>
         <Container>
-          <h2>Find your favorite pet</h2>
+          <Wrapper>
+            <Title>Find your favorite pet</Title>
+          </Wrapper>
           <NoticesSearch />
           <NoticesCategoriesNav />
           <NoticesCategoriesList />
