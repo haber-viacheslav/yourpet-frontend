f3cda1ba5dcb4e1b43031531df60688c227c8254
TM <perebziak.vladislav@gmail.com>
2023-05-25 11:36:53 +0300

    add refreshing

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Formik, ErrorMessage } from 'formik';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { logIn } from '../../redux/auth/authService';
+import { logIn, refreshTokens } from '../../redux/auth/authService';
 import { string, object } from 'yup';
 import { iconClose, iconOpen, IconCrossSmall, IconCheck } from './Icons/Icons';
 import {
@@ -19,15 +19,51 @@ import {
   Link,
   EmailMessage,
   PasswordMessage,
-  SuccessMessage
+  SuccessMessage,
 } from './LoginForm.styled';
+import { refreshingTokensExecutor } from 'helpers/refreshingTokensExecutor';
 
 export const LoginForm = () => {
   const [toggleIconPass, setToggleIconPass] = useState(iconClose);
   const [typePass, setTypePass] = useState('password');
 
   const dispatch = useDispatch();
+  // ACHTUNG
+  // THIS useEffect is created ONLY AS AN EXAMPLE!!!!!
+  useEffect(() => {
+    // IIEF async func or named async func which doing request to some endpoints
+    (async () => {
+      try {
+        // only the catch block trigger
+        const exampleONLY = new Promise((resolve, reject) => {
+          reject();
+        });
+        exampleONLY();
+        // const resp = await  functionToRequestSomeData()
+        // other logic...
+        // if access token has expired the server send 403 error and the catch block will be triggered.
+      } catch (error) {
+        /** //refreshingTokensExecutor//
+         *  @param {error}
+         * catch error body
+         * @param { dispatch} *link to the dispatch
+         * @param { action} *an action that using in dispatch, like "dispatch(someAction())"
+         */
+        const result = refreshingTokensExecutor({
+          error: {
+            // This path is an EXAMPLE ONLY , send ONLY the catch error obj in the arguments of this func!!!
+            response: { data: { code: 403, message: 'expired' } },
+          },
+          dispatch,
+          action: refreshTokens,
+        });
+        console.log(result); // if error 403 result would be "expired" (type String) and you could do some logic if you want
+        // if error has  any codes except 403 the result would be "error" (type Object)!!
+      }
+    })();
 
+    // dispatch(refreshTokens());
+  }, [dispatch]);
   const yupLoginValidation = object().shape({
     email: string().email('Enter a Valid Email').required('Email is Required'),
     password: string()
@@ -116,7 +152,8 @@ export const LoginForm = () => {
                     <PasswordMessage>{password}</PasswordMessage>
                   )}
                 />
-              )}  {touched.password && !errors.password && (
+              )}{' '}
+              {touched.password && !errors.password && (
                 <SuccessMessage>Password is secure</SuccessMessage>
               )}
             </Box>
