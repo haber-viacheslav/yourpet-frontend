a79317e30182b88b96299760b6eb572c4ceb2b62
TM <perebziak.vladislav@gmail.com>
2023-05-25 15:33:51 +0300

    fix

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -21,49 +21,26 @@ import {
   PasswordMessage,
   SuccessMessage,
 } from './LoginForm.styled';
-import { refreshingTokensExecutor } from 'helpers/refreshingTokensExecutor';
+import { fetchDecorator } from 'helpers/fetchDecorator';
+import axios from 'axios';
 
 export const LoginForm = () => {
   const [toggleIconPass, setToggleIconPass] = useState(iconClose);
   const [typePass, setTypePass] = useState('password');
 
   const dispatch = useDispatch();
-  // ACHTUNG
-  // THIS useEffect is created ONLY AS AN EXAMPLE!!!!!
+  // EXAMPLE
   useEffect(() => {
-    // IIEF async func or named async func which doing request to some endpoints
+    // use async IIEF or named  async func
     (async () => {
-      try {
-        // only the catch block trigger
-        const exampleONLY = new Promise((resolve, reject) => {
-          reject();
-        });
-        exampleONLY();
-        // const resp = await  functionToRequestSomeData()
-        // other logic...
-        // if access token has expired the server send 403 error and the catch block will be triggered.
-      } catch (error) {
-        /** //refreshingTokensExecutor//
-         *  @param {error}
-         * catch error body
-         * @param { dispatch} *link to the dispatch
-         * @param { action} *an action that using in dispatch, like "dispatch(someAction())"
-         */
-        const result = refreshingTokensExecutor({
-          error: {
-            // This path is an EXAMPLE ONLY , send ONLY the catch error obj in the arguments of this func!!!
-            response: { data: { code: 403, message: 'expired' } },
-          },
-          dispatch,
-          action: refreshTokens,
-        });
-        console.log(result); // if error 403 result would be "expired" (type String) and you could do some logic if you want
-        // if error has  any codes except 403 the result would be "error" (type Object)!!
-      }
+      const resp = await fetchDecorator(dispatch, refreshTokens, () =>
+        axios.get('news')
+      );
+      console.log(resp);
     })();
-
-    // dispatch(refreshTokens());
   }, [dispatch]);
+  // -----------------------------------
+
   const yupLoginValidation = object().shape({
     email: string().email('Enter a Valid Email').required('Email is Required'),
     password: string()
@@ -170,3 +147,40 @@ export const LoginForm = () => {
     </Wrapper>
   );
 };
+
+// ACHTUNG
+// THIS useEffect is created ONLY AS AN EXAMPLE!!!!!
+// useEffect(() => {
+//   // IIEF async func or named async func which doing request to some endpoints
+//   (async () => {
+//     try {
+//       // only the catch block trigger
+//       const exampleONLY = new Promise((resolve, reject) => {
+//         reject();
+//       });
+//       exampleONLY();
+//       // const resp = await  functionToRequestSomeData()
+//       // other logic...
+//       // if access token has expired the server send 403 error and the catch block will be triggered.
+//     } catch (error) {
+//       /** //refreshingTokensExecutor//
+//        *  @param {error}
+//        * catch error body
+//        * @param { dispatch} *link to the dispatch
+//        * @param { action} *an action that using in dispatch, like "dispatch(someAction())"
+//        */
+//       const result = refreshingTokensExecutor({
+//         error: {
+//           // This path is an EXAMPLE ONLY , send ONLY the catch error obj in the arguments of this func!!!
+//           response: { data: { code: 403, message: 'expired' } },
+//         },
+//         dispatch,
+//         action: refreshTokens,
+//       });
+//       console.log(result); // if error 403 result would be "expired" (type String) and you could do some logic if you want
+//       // if error has  any codes except 403 the result would be "error" (type Object)!!
+//     }
+//   })();
+
+//   // dispatch(refreshTokens());
+// }, [dispatch]);
