94fd77bee900883f586add05400de1f4b3dd7513
VitaliySaburdo <vsaburdo@gmail.com>
2023-05-25 10:53:24 +0300

    1

--- src/components/Navigation/AuthNav/AuthNav.styled.jsx
+++ src/components/Navigation/AuthNav/AuthNav.styled.jsx
@@ -18,8 +18,6 @@ export const AuthNavMenu = styled.div`
 `;
 
 export const RegisterLink = styled(NavLink)`
-  position: relative;
-  z-index: 1000;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -36,8 +34,6 @@ export const RegisterLink = styled(NavLink)`
 `;
 
 export const LoginLink = styled(NavLink)`
-  position: relative;
-  z-index: 1000;
   margin-bottom: 20px;
   display: flex;
   justify-content: center;
