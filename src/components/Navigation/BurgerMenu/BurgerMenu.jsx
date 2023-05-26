b955e8af545076f2f477a49f59f20513f461f60a
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-05-26 17:37:44 +0200

    correct style css

--- src/components/Navigation/BurgerMenu/BurgerMenu.jsx
+++ src/components/Navigation/BurgerMenu/BurgerMenu.jsx
@@ -32,23 +32,15 @@ export const BurgerMenu = () => {
       document.removeEventListener('mousedown', handler);
     };
   }, []);
-
+  
   useEffect(() => {
-    const scroleStop = e => {
-      const backdrop = document.querySelector('body');
-
-      if (isOpen) {
-        backdrop.style.overflow = "hidden";
-      }else{
-        backdrop.style.overflow = "visible";
-      }
-    };
-    window.addEventListener('mousemove', scroleStop);
-    return () => {
-    window.removeEventListener('mousemove', scroleStop);
-    };
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'visible';
+    }
   }, [isOpen]);
-
+  
   return (
     <>
       <Burger>
