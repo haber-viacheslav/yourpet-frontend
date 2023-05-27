e3162646f4f15ff70e5c9b33a6516af8dada5e52
Vladislav-UZH <perebziak.vladislav@gmail.com>
2023-05-27 10:27:29 +0300

    FIXEDcode .code .code .

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,43 +3,30 @@ import { localStorageService } from 'helpers/localStorageService';
 const baseURL = 'https://your-pet-api.onrender.com/api/v1';
 axios.defaults.baseURL = baseURL;
 // export const instance = axios.create({ baseURL });
-export const setToken = async token => {
+export const setToken = token => {
   if (!token) {
-    return (axios.defaults.headers.common.authorization = ``);
+    return (axios.defaults.headers.Authorization = ``);
   }
-  return (axios.defaults.headers.common.authorization = `Bearer ${token}`);
+
+  return (axios.defaults.headers.Authorization = `Bearer ${token}`);
 };
-// INTERCEPTORS
-// axios.interceptors.request.use(config => {
-//   const accessToken = localStorageService.getItem('accessToken');
-//   config.headers.common.authorization = `Bearer ${accessToken}`;
-//   return config;
-// });
+
 axios.interceptors.response.use(
   resp => resp,
   async error => {
     console.log(error.response.data.code);
     if (
-      error.response.data.code === 403
-      // || error.response.data.message.includes('authorization')
+      error.response.data.code === 403 ||
+      error.response.data.message.includes('authorization')
     ) {
       const oldRefreshToken = localStorageService.getItem('refreshToken');
-      // console.log('OLD REFRESH', oldRefreshToken);
       try {
         const { data } = await axios.post('/auth/refresh', {
           refreshToken: oldRefreshToken,
         });
         const { accessToken, refreshToken } = data.body;
-        console.log(
-          'NEW ACCESS TOKEN ==== OLD ACCESS TOKEN',
-          accessToken === axios.defaults.headers.common.authorization
-        );
-
-        console.log('NEW ACCESS TOKEN!!!!!!!', accessToken);
-        console.log('NEW refresh TOKEN!!!!!!!', refreshToken);
-        // console.log('NEW === OLD REFRESH', oldRefreshToken === refreshToken);
-        setToken(accessToken);
         localStorageService.setItem('refreshToken', refreshToken);
+        error.config.headers.Authorization = `Bearer ${accessToken}`;
         return axios(error.config);
       } catch (error) {
         return Promise.reject(error);
@@ -53,7 +40,6 @@ axios.interceptors.response.use(
 export const registerFetch = async credentials => {
   const { data } = await axios.post('/auth/register', credentials);
   const { accessToken, refreshToken } = data.body;
-  console.log(data);
   setToken(accessToken);
   localStorageService.setItem('refreshToken', refreshToken);
   return data;
