9b5d1ff01cc7baadc1779c44e3f0de3b41f4699c
DarinaPilganchuk <106483724+DarinaPilganchuk@users.noreply.github.com>
2023-05-28 19:49:33 +0300

    render btn noticeNav

--- src/components/Notices/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/Notices/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -1,41 +1,83 @@
-import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { selectIsLoggedIn } from 'redux/auth/selectors';
 import {
-  BtnAddOption,
   BtnAddPet,
+  BtnFiltersCircle,
 } from 'components/buttons/buttons';
 import {
-  FilterAdsContainer,
   FiltersBtnContainer,
   FiltersContainer,
+  StyledNavLink,
+  NavList,
 } from './NoticesCategoriesNav.styled';
 import { FilterCards } from '../FilterCards/FilterCards';
+import styled from 'styled-components';
 
-export const NoticesCategoriesNav = () => {
+const Link = [
+  { to: 'sell', text: 'sell' },
+  { to: 'lost-found', text: 'lost/found' },
+  { to: 'for-free', text: 'in good hands' },
+];
+
+const getLink = ({ isActive }) => {
+  const className = isActive ? `${StyledNavLink} ${styled.active}` : StyledNavLink;
+  return className;
+};
+export const NoticesCategoriesNav = ({ onOwnClick, onFavoriteClick }) => {
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+   const handleClick = () => {
+    if (!isLoggedIn) {
+      //  ('You must be logged in');
+      return;
+    } else {
+      navigate('/add-pet');
+    }
+  };
+  
+
   return (
-    <>
       <FiltersContainer>
-        <NavLink to={'/notices/sell'}>
-          <BtnAddOption text={'sell'} />
-        </NavLink>
-        <NavLink to={'/notices/lost-found'}>
-          <BtnAddOption text={'lost/found'} />
-        </NavLink>
-        <NavLink to={'/notices/for-free'}>
-          <BtnAddOption text={'in good hands'} />
-        </NavLink>
-        <FilterAdsContainer>
-          <NavLink to={'/notices/favorite'}>
-            <BtnAddOption text={'favorite ads'} />
-          </NavLink>
-        </FilterAdsContainer>
-        <NavLink to={'/notices/own'}>
-          <BtnAddOption text={'my ads'} />
-        </NavLink>
+          <NavList>
+        {Link.map(element => (
+            <StyledNavLink
+              to={element.to}
+              className={getLink}
+            >
+              {element.text}
+            </StyledNavLink>
+          
+        ))}
+        {isLoggedIn && (
+          <>
+              <StyledNavLink
+                to="own"
+                className={getLink}
+                onClick={() => {
+                  onOwnClick();
+                }}
+              >
+                my ads
+              </StyledNavLink>
+              <StyledNavLink
+                to="favorite"
+                className={getLink}
+                onClick={() => {
+                  onFavoriteClick();
+                }}
+              >
+                favorite ads
+              </StyledNavLink>
+          </>
+        )}
+      </NavList>
         <FiltersBtnContainer>
           <FilterCards />
-          <BtnAddPet />
+          <BtnAddPet onClick={handleClick} />
         </FiltersBtnContainer>
+        <BtnFiltersCircle />
       </FiltersContainer>
-    </>
   );
 };
