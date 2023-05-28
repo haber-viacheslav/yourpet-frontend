da5e303f11b5cbcd0a252812c76027c4c214bdc7
AH400796 <a.hokhman@gmail.com>
2023-05-28 22:01:27 +0300

    123

--- src/pages/NoticesPage.jsx
+++ src/pages/NoticesPage.jsx
@@ -1,21 +1,29 @@
+import { useState } from 'react';
+
 import { Container } from '../components/Container/Container';
 import { Section } from '../components/Section/Section';
-import { NoticesCategoriesNav } from 'components/Notices/NoticesCategoriesNav/NoticesCategoriesNav';
-import { NoticesSearch } from 'components/Notices/NoticesSearch/NoticesSearch';
+import { NoticesCategoriesNav } from 'components/Notices (new)/NoticesCategoriesNav/NoticesCategoriesNav';
+import { NoticesSearch } from 'components/Notices (new)/NoticesSearch/NoticesSearch';
 import { Title, Wrapper } from 'components/Friends/Friends.styled';
-import { NoticesCategoriesList } from 'components/Notices/NoticesCategoriesList/NoticesCategoriesList';
+import { NoticesCategoriesList } from 'components/Notices (new)/NoticesCategoriesList/NoticesCategoriesList';
 
 const NoticesPage = () => {
+  const [category, setCategory] = useState('');
+
+  const handleChoose = option => {
+    setCategory(option);
+  };
+
   return (
     <>
       <Section>
         <Container>
           <Wrapper>
-            <Title>Find your favorite pet</Title>
+            <Title>Find your favorite pet{category}</Title>
           </Wrapper>
           <NoticesSearch />
-          <NoticesCategoriesNav />
-          <NoticesCategoriesList />
+          <NoticesCategoriesNav onChoosingCategory={handleChoose} />
+          <NoticesCategoriesList category={category} />
         </Container>
       </Section>
     </>
